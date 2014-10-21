fda90063b1e17d154742294a4b2a69b12a9e6bba
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-20 17:57:24 -0700

    First commit of Imaginarium

---
+++ ImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by Anthony Dagati on 10/20/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *imageURL;
+
+@end
---
+++ ImageViewController.m
@@ -0,0 +1,42 @@
+//
+//  ImageViewController.m
+//  Imaginarium
+//
+//  Created by Anthony Dagati on 10/20/14.
+//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController ()
+
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIImage *image;
+
+@end
+
+@implementation ImageViewController
+
+-(UIImageView *)imageView
+{
+    if (!_imageView) _imageView = [[UIImageView alloc] init];
+    return _imageView;
+}
+
+-(UIImage *)image{
+    return self.imageView.image;
+}
+
+-(void)setImage:(UIImage *)image
+{
+    self.imageView.image = image;
+    [self.imageView sizeToFit];
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.view addSubview:self.imageView];
+    
+}
+@end
